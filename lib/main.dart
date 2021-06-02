c0117616ff5a8a99a221e7ab0317841372533752
khalil <khalil.pan2@gmail.com>
2021-06-02 10:09:32 -0300

    Inicial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Expense Planner')),
+      ),
+      body: Center(
+        child: Text('Body'),
+      ),
+    );
+  }
+}
