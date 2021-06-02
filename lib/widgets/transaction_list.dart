14bcc211fea00bb0668a9293767ba18d7c651934
khalil <khalil.pan2@gmail.com>
2021-06-02 16:41:54 -0300

    adding Listview.builder, ListView(chldren : []) to the transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,54 +8,168 @@ class TransactionList extends StatelessWidget {
 
   TransactionList({required this.userTransactions});
 
+//solution with ListView.builder
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransactions.map((item) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'R\$ ${item.amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    item.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+    return Container(
+      height: 370,
+      child: ListView.builder(
+        itemBuilder: (contex, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                  Text(
-                    DateFormat().format(item.date),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${userTransactions[index].amount}',
                     style: TextStyle(
-                      color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      userTransactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                    Text(
+                      DateFormat().format(userTransactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: userTransactions.length,
+      ),
     );
   }
 }
+
+//solution with ListView(chldren : [])
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 370,
+//       child: ListView(
+//         children: userTransactions.map((item) {
+//           return Card(
+//             child: Row(
+//               children: <Widget>[
+//                 Container(
+//                   margin: EdgeInsets.symmetric(
+//                     vertical: 10,
+//                     horizontal: 15,
+//                   ),
+//                   decoration: BoxDecoration(
+//                       border: Border.all(
+//                     color: Colors.purple,
+//                     width: 2,
+//                   )),
+//                   padding: EdgeInsets.all(10),
+//                   child: Text(
+//                     'R\$ ${item.amount}',
+//                     style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 20,
+//                         color: Colors.purple),
+//                   ),
+//                 ),
+//                 Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: <Widget>[
+//                     Text(
+//                       item.title,
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 16,
+//                       ),
+//                     ),
+//                     Text(
+//                       DateFormat().format(item.date),
+//                       style: TextStyle(
+//                         color: Colors.grey,
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
+
+//solution with column
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: userTransactions.map((item) {
+//         return Card(
+//           child: Row(
+//             children: <Widget>[
+//               Container(
+//                 margin: EdgeInsets.symmetric(
+//                   vertical: 10,
+//                   horizontal: 15,
+//                 ),
+//                 decoration: BoxDecoration(
+//                     border: Border.all(
+//                   color: Colors.purple,
+//                   width: 2,
+//                 )),
+//                 padding: EdgeInsets.all(10),
+//                 child: Text(
+//                   'R\$ ${item.amount}',
+//                   style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 20,
+//                       color: Colors.purple),
+//                 ),
+//               ),
+//               Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: <Widget>[
+//                   Text(
+//                     item.title,
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 16,
+//                     ),
+//                   ),
+//                   Text(
+//                     DateFormat().format(item.date),
+//                     style: TextStyle(
+//                       color: Colors.grey,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             ],
+//           ),
+//         );
+//       }).toList(),
+//     );
+//   }
+// }
